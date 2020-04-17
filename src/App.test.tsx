c2dfe9fc34ef23a995bfac93bb66845b4ad1610f
Jo Sprague <josiah.sprague@gmail.com>
2020-04-17 12:30:57 +0100

    Update simple test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders San Francisco Street Food heading', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = getByText(/San Francisco Street Food/i);
+  expect(heading).toBeInTheDocument();
 });
