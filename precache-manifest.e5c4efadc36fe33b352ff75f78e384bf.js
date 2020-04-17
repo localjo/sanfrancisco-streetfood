fb1439a89c63dcc2d592121ce61cd75b921ee1b8
Jo Sprague <josiah.sprague@gmail.com>
2020-04-17 11:54:37 +0100

    Updates

---
+++ precache-manifest.e5c4efadc36fe33b352ff75f78e384bf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b015e4e3677e50aa203772e994ecacb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43146a9049cb51ed47fd",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "eb5104dffd9242fc277a",
+    "url": "/static/js/2.7ca9a2f9.chunk.js"
+  },
+  {
+    "revision": "33ade7f87243ebea1c83e915f94afce3",
+    "url": "/static/js/2.7ca9a2f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43146a9049cb51ed47fd",
+    "url": "/static/js/main.b9f142f3.chunk.js"
+  },
+  {
+    "revision": "a3f784c0f097f6daa782",
+    "url": "/static/js/runtime-main.d1212384.js"
+  },
+  {
+    "revision": "233709b1a56d0cd903dc66ead313310a",
+    "url": "/static/media/food.233709b1.svg"
+  },
+  {
+    "revision": "cf2c79cfb492220baab9cc834807675e",
+    "url": "/static/media/map.cf2c79cf.svg"
+  },
+  {
+    "revision": "bf6159db74ed37c63f5fc76091f72ecd",
+    "url": "/static/media/pin.bf6159db.svg"
+  },
+  {
+    "revision": "8bfbd4464c234ca564827c2dda9f5cf3",
+    "url": "/static/media/table.8bfbd446.svg"
+  }
+]);
